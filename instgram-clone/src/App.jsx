3a997839f53d357ab88f70897656cd04d6d244e0
AbdouAoufi <Abdouou7@gmail.com>
2021-09-10 12:57:17 +0100

    init tailwind with vite

---
+++ instgram-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+       <p className="font-bold">text</p>
+    </div>
+  )
+}
+
+export default App
